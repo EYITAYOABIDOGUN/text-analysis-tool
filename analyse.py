37759070096dac429bc5354ad865325fae17385d
eyitayo <eyitayo@EYITAYO.localdomain>
2025-12-30 14:10:14 +0100

    obtained username from terminal input

---
+++ analyse.py
@@ -0,0 +1,9 @@
+print("\nWelcome to the text analysis tool, I will mine and analyze a body of text from")
+
+# Get Username
+
+# Print message prompting user to input their name
+username = input("\nTo begin, please enter your username:\n")
+
+# Greet the user
+print("Hello, " + username)
