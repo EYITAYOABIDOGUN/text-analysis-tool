ed83fcb5a61b8dc01e99815ba4339e01ad53d532
eyitayo <eyitayo@EYITAYO.localdomain>
2026-01-23 15:43:58 +0100

    Completed file readbility capability

--- analyse.py
+++ analyse.py
@@ -42,8 +42,18 @@ def getUsername():
 # Greet the user
 def greetUser(name):
     print("Hello, " + name)
-
+# Get text from file 
+def getArticleText():
+    f = open("files/article.txt", "r")
+    rawText = f.read()
+    f.close()
+    return rawText.replace("\n", "").replace("\r", "")
 
 welcomeUser()
 username = getUsername()
 greetUser(username)
+
+
+articleTextRaw = getArticleText()
+print("GOT:")
+print(getArticleText())
